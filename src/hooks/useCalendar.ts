42745bc1da456bf9eed56a1e859c963bfffcf28a
Fer457 <fernandonevothernandez@gmail.com>
2024-03-13 13:27:31 +0100

    Send push token to backend

--- src/hooks/useCalendar.ts
+++ src/hooks/useCalendar.ts
@@ -12,7 +12,7 @@ const useCalendar = () => {
 
     try {
       const accessToken = await AsyncStorage.getItem("accestoken");
-      console.log(`Token obtenido: ${accessToken}`);
+      // console.log(`Token obtenido: ${accessToken}`);
 
       const response = await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
         method: "GET",
@@ -22,13 +22,13 @@ const useCalendar = () => {
         },
       });
       const data = await response.json();
-      console.log("Datos recibidos de la API");
+      // console.log("Datos recibidos de la API");
       setEvents(data.items);
     } catch (err: any) {
       console.error("Error en refetch", err);
       setError(err.message);
     } finally {
-      console.log("Refetch finalizado - setLoading a false");
+      // console.log("Refetch finalizado - setLoading a false");
       setLoading(false);
     }
   }, []);
